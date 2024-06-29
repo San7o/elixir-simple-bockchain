403ea4e5e5eb3c7ed21be8b52a473d3b7a7aae04
San7o <santigio2003@gmail.com>
2024-06-29 13:35:52 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Elixir";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        toolchain = "latest";
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            act
+            elixir
+            erlang_27
+          ];
+
+          shellHook = ''
+          '';
+        };
+      });
+}
